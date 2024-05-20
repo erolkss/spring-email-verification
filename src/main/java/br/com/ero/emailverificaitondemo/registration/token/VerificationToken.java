8bd83c5bdfe8f80ac6d6bf5aa99831acaa053d5f
Lucas Lima <lucaserolima@gmail.com>
2024-05-19 23:23:29 -0400

    feat: Create a VerificationToken entity and repository.

---
+++ src/main/java/br/com/ero/emailverificaitondemo/registration/token/VerificationToken.java
@@ -0,0 +1,50 @@
+package br.com.ero.emailverificaitondemo.registration.token;
+
+import br.com.ero.emailverificaitondemo.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class VerificationToken {
+
+
+    private static final int EXPIRATION_TIME = 15;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Date expirationTime;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public VerificationToken(String token, User user) {
+        super();
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public VerificationToken(String token) {
+        super();
+        this.token = token;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public Date getTokenExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calendar.getTime().getTime());
+    }
+}
