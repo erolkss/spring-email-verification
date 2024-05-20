9e2653967ab904c64d17924aba6d6660aaea49d7
Lucas Lima <lucaserolima@gmail.com>
2024-05-19 23:22:51 -0400

    feat: Create a Registration Controller and created the method for user registration and within it trigger a 'RegistrationCompleteEvent' event

---
+++ src/main/java/br/com/ero/emailverificaitondemo/registration/RegistrationController.java
@@ -0,0 +1,33 @@
+package br.com.ero.emailverificaitondemo.registration;
+
+import br.com.ero.emailverificaitondemo.event.RegistrationCompleteEvent;
+import br.com.ero.emailverificaitondemo.user.User;
+import br.com.ero.emailverificaitondemo.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final UserService userService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @PostMapping
+    public String registerUser(RegistrationRequest registrationRequest, final HttpServletRequest servletRequest) {
+        User user = userService.registerUser(registrationRequest);
+
+        applicationEventPublisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(servletRequest)));
+
+        return "Success! Please check your email to complete your registration!";
+    }
+
+    public String applicationUrl(HttpServletRequest servletRequest) {
+        return "http://" + servletRequest.getServerName() + ":" + servletRequest.getServerPort() + servletRequest.getContextPath();
+    }
+}
