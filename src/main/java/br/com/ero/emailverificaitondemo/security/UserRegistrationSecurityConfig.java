5f27c749e6c083c7e1d4bcf3676ec5b7b072a129
Lucas Lima <lucaserolima@gmail.com>
2024-05-20 19:22:19 -0400

    feat: Implemented the method with SecurityFilterChain to configure Spring Security security filters

--- src/main/java/br/com/ero/emailverificaitondemo/security/UserRegistrationSecurityConfig.java
+++ src/main/java/br/com/ero/emailverificaitondemo/security/UserRegistrationSecurityConfig.java
@@ -2,9 +2,13 @@ package br.com.ero.emailverificaitondemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +17,16 @@ public class UserRegistrationSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/register").permitAll();
+                    request.requestMatchers("/users")
+                            .hasAnyAuthority("USER", "ADMIN");
+                }).formLogin(Customizer.withDefaults()).build();
     }
 }
