c10719b9c6eef668d09a7ac2bbf83bd9f47f3309
Lucas Lima <lucaserolima@gmail.com>
2024-05-19 21:31:22 -0400

    First Commit

---
+++ src/test/java/br/com/ero/emailverificaitondemo/EmailVerificaitonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.emailverificaitondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerificaitonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
